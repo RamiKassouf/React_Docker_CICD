fad3a49272c47c43a5402a6b17589274225f01d6
Rami Kassouf <Rami_kassouf@hotmail.com>
2023-09-19 12:07:10 +0300

    added react app and github actions

---
+++ test/test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
+    
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello World!" in response.data
